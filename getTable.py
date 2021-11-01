2b574288e0af501b506d93a7966ddcc879f23597
mysper <anthony.a3203@gmail.com>
2021-11-01 16:28:36 +0000

    1

---
+++ getTable.py
@@ -0,0 +1,16 @@
+
+
+
+'''
+def getIndex(c):
+  try:
+    return index.index(c)
+  except:
+    return -1
+
+def getValue(a,b):
+  index_a = getIndex(a)
+  index_b = getIndex(b)
+#  if index_a < 0 or index_b < 0:
+  return values[index_a][index_b]
+'''
--- main.py
+++ main.py
@@ -12,18 +12,6 @@ for v in payload:
   temp = v[:-1].split(',')
   values.append(temp)
 
-def getIndex(c):
-  try:
-    return index.index(c)
-  except:
-    return -1
-
-def getValue(a,b):
-  index_a = getIndex(a)
-  index_b = getIndex(b)
-#  if index_a < 0 or index_b < 0:
-  return values[index_a][index_b]
-
 
 _json = {
   'index': index,
